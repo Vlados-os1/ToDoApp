d135e772840523bdbefab35abd2cae0102979776
Vlados-os1 <boatslav228@gmail.com>
2025-04-12 22:04:13 +0300

    cope from his repo

--- main.py
+++ main.py
@@ -7,25 +7,29 @@ from fastapi.middleware.cors import CORSMiddleware
 from models import init_db
 import requests as rq
 
+
 class AddTask(BaseModel):
     tg_id: int
     title: str
+
 
 class CompleteTask(BaseModel):
     id: int
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app_: FastAPI):
     await init_db()
-    print("Bot is ready")
+    print('Bot is ready')
     yield
+
 
 app = FastAPI(title="To Do App", lifespan=lifespan)
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -42,13 +46,15 @@ async def tasks(tg_id: int):
 async def profile(tg_id: int):
     user = await rq.add_user(tg_id)
     completed_tasks_count = await rq.get_completed_tasks_count(user.id)
-    return {"completedTasks": completed_tasks_count}
+    return {'completedTasks': completed_tasks_count}
+
 
 @app.post("/api/add")
 async def add_task(task: AddTask):
     user = await rq.add_user(task.tg_id)
     await rq.add_task(user.id, task.title)
     return {'status': 'ok'}
+
 
 @app.patch("/api/completed")
 async def complete_task(task: CompleteTask):
--- requests.py
+++ requests.py
@@ -1,20 +1,16 @@
-from turtledemo.paint import switchupdown
-
-from matplotlib.pyplot import title
 from sqlalchemy import select, update, delete, func
 from models import async_session, User, Task
 from pydantic import BaseModel, ConfigDict
-from typing import List
 
 
-
-class Task_Schema(BaseModel):
+class TaskSchema(BaseModel):
     id: int
     title: str
     completed: bool
     user: int
 
     model_config = ConfigDict(from_attributes=True)
+
 
 async def add_user(tg_id):
     async with async_session() as session:
@@ -26,7 +22,8 @@ async def add_user(tg_id):
         session.add(new_user)
         await session.commit()
         await session.refresh(new_user)
-        return user
+        return new_user
+
 
 async def get_tasks(user_id):
     async with async_session() as session:
@@ -35,12 +32,16 @@ async def get_tasks(user_id):
         )
 
         serialized_tasks = [
-            Task_Schema.model_validate(t).model_dump() for t in tasks
+            TaskSchema.model_validate(t).model_dump() for t in tasks
         ]
+
+        return serialized_tasks
+
 
 async def get_completed_tasks_count(user_id):
     async with async_session() as session:
-        return await session.scalar(select(func.coint(Task.id)).where(Task.completed == True))
+        return await session.scalar(select(func.count(Task.id)).where(Task.completed == True))
+
 
 async def add_task(user_id, title):
     async with async_session() as session:
@@ -54,5 +55,5 @@ async def add_task(user_id, title):
 
 async def update_task(task_id):
     async with async_session() as session:
-        await session.execute(update(Task).where(Task.id == task_id).value(completed=True))
+        await session.execute(update(Task).where(Task.id == task_id).values(completed=True))
         await session.commit()
