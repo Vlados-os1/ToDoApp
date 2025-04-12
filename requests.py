8d3fe0ece5ea1a68705a86e970e9354e8c6ffc43
Vlados-os1 <boatslav228@gmail.com>
2025-04-12 20:45:08 +0300

    Initial commit

---
+++ requests.py
@@ -0,0 +1,58 @@
+from turtledemo.paint import switchupdown
+
+from matplotlib.pyplot import title
+from sqlalchemy import select, update, delete, func
+from models import async_session, User, Task
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+
+
+class Task_Schema(BaseModel):
+    id: int
+    title: str
+    completed: bool
+    user: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+async def add_user(tg_id):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        if user:
+            return user
+
+        new_user = User(tg_id=tg_id)
+        session.add(new_user)
+        await session.commit()
+        await session.refresh(new_user)
+        return user
+
+async def get_tasks(user_id):
+    async with async_session() as session:
+        tasks = await session.scalars(
+            select(Task).where(Task.user == user_id, Task.completed == False)
+        )
+
+        serialized_tasks = [
+            Task_Schema.model_validate(t).model_dump() for t in tasks
+        ]
+
+async def get_completed_tasks_count(user_id):
+    async with async_session() as session:
+        return await session.scalar(select(func.coint(Task.id)).where(Task.completed == True))
+
+async def add_task(user_id, title):
+    async with async_session() as session:
+        new_task = Task(
+            title=title,
+            user=user_id
+        )
+        session.add(new_task)
+        await session.commit()
+
+
+async def update_task(task_id):
+    async with async_session() as session:
+        await session.execute(update(Task).where(Task.id == task_id).value(completed=True))
+        await session.commit()
